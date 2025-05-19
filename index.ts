db14009bc6bc363e60c2eb6e5ab7f27cdf5661c0
blueGyu <bluegyufordev@gmail.com>
2025-05-19 10:41:06 +0900

    refactor: index.js 타입스크립트로 마이그레이션

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-const app = require("./server");
+import app from "./server";
 
-const port = process.env.PORT || 8000;
+const port = parseInt(process.env.PORT ?? "8000");
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port: ${port}`);
 });
