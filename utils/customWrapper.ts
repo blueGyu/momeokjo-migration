66f13c3863a55856cdd92b8797315716f375c14b
blueGyu <bluegyufordev@gmail.com>
2025-05-25 04:10:06 +0900

    refactor: wrapper 리턴 타입 수정

--- utils/customWrapper.ts
+++ utils/customWrapper.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, RequestHandler } from "express";
 import { Pool, PoolClient } from "pg";
 
 // 일반 wrapper 타입
@@ -14,7 +14,7 @@ type AsyncRequestHandlerWithDb = (
 
 // 일반 wrapper
 export const tryCatchWrapper =
-  (func: AsyncRequestHandler): AsyncRequestHandler =>
+  (func: AsyncRequestHandler): RequestHandler =>
   async (req, res, next) => {
     try {
       await func(req, res, next);
@@ -26,7 +26,7 @@ export const tryCatchWrapper =
 // 데이터베이스 작업이 필요한 경우 사용할 wrapper
 export const tryCatchWrapperWithDb =
   (pool: Pool) =>
-  (func: AsyncRequestHandlerWithDb): AsyncRequestHandlerWithDb =>
+  (func: AsyncRequestHandlerWithDb): RequestHandler =>
   async (req, res, next) => {
     let client: PoolClient | undefined;
     try {
@@ -44,7 +44,7 @@ export const tryCatchWrapperWithDb =
 // 신고 누적으로 인한 soft delete 진행 시 사용할 wrapper
 export const tryCatchWrapperWithDbTransaction =
   (pool: Pool) =>
-  (func: AsyncRequestHandlerWithDb): AsyncRequestHandlerWithDb =>
+  (func: AsyncRequestHandlerWithDb): RequestHandler =>
   async (req, res, next) => {
     let client: PoolClient | undefined;
     try {
