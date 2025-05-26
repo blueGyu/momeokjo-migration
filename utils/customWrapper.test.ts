c7ba07b8a95930935ae852550d6a2f0c74dac899
blueGyu <bluegyufordev@gmail.com>
2025-05-26 16:52:23 +0900

    refactor: customWrapper 테스트 코드 수정
    - 데이터베이스 모킹 중복 제거
    - 누락된 기대값 추가
    - 테스트 설명 수정

--- utils/customWrapper.test.ts
+++ utils/customWrapper.test.ts
@@ -1,125 +1,120 @@
-import { NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
+import { PoolClient } from "pg";
+
+import pool from "../database/db";
 import {
   tryCatchWrapper,
   tryCatchWrapperWithDb,
   tryCatchWrapperWithDbTransaction,
 } from "./customWrapper";
-import { Pool, PoolClient } from "pg";
+
+jest.mock("../database/db", () => ({
+  connect: jest.fn().mockResolvedValue({
+    release: jest.fn(),
+    query: jest.fn(),
+  }),
+}));
 
 const req = {} as Request;
 const res = {} as Response;
 const next = jest.fn() as unknown as NextFunction;
 
+let mockClient: PoolClient;
+beforeEach(async () => {
+  jest.resetModules();
+  mockClient = await pool.connect();
+});
+
 describe("tryCatchWrapper", () => {
-  it("정상 동작 시 next 호출되지 않아야 함", async () => {
-    const mockHandler = jest.fn().mockResolvedValue(undefined);
+  it("정상 동작 시 next 호출되지 않아야 한다.", async () => {
+    const mockFunc = jest.fn().mockResolvedValue(undefined);
 
-    await tryCatchWrapper(mockHandler)(req, res, next);
+    await tryCatchWrapper(mockFunc)(req, res, next);
 
-    expect(mockHandler).toHaveBeenCalled();
+    expect(mockFunc).toHaveBeenCalledWith(req, res, next);
     expect(next).not.toHaveBeenCalled();
   });
 
-  it("에러 발생 시 next 호출되어야 함", async () => {
+  it("에러 발생 시 next 호출되어야 한다.", async () => {
     const error = new Error("test error");
-    const mockHandler = jest.fn().mockRejectedValue(error);
+    const mockFunc = jest.fn().mockRejectedValue(error);
 
-    await tryCatchWrapper(mockHandler)(req, res, next);
+    await tryCatchWrapper(mockFunc)(req, res, next);
 
+    expect(mockFunc).toHaveBeenCalledWith(req, res, next);
     expect(next).toHaveBeenCalledWith(error);
   });
 });
 
 describe("tryCatchWrapperWithDb", () => {
-  it("DB 연결 후 핸들러 실행", async () => {
-    const mockClient = { release: jest.fn() } as unknown as PoolClient;
-    const mockPool = { connect: jest.fn().mockResolvedValue(mockClient) } as unknown as Pool;
-    const mockHandler = jest.fn().mockResolvedValue(undefined);
+  it("DB 연결 후 func를 실행해야한다.", async () => {
+    const mockFunc = jest.fn().mockResolvedValue(undefined);
 
-    await tryCatchWrapperWithDb(mockPool)(mockHandler)(req, res, next, mockClient);
+    await tryCatchWrapperWithDb(pool)(mockFunc)(req, res, next);
 
-    expect(mockPool.connect).toHaveBeenCalled();
-    expect(mockHandler).toHaveBeenCalledWith(req, res, next, mockClient);
+    expect(pool.connect).toHaveBeenCalled();
+    expect(mockFunc).toHaveBeenCalledWith(req, res, next, mockClient);
     expect(mockClient.release).toHaveBeenCalled();
   });
 
-  it("에러 발생 시 next 호출", async () => {
+  it("에러 발생 시 next 호출해야한다.", async () => {
     const error = new Error("db error");
-    const mockClient = { release: jest.fn() } as unknown as PoolClient;
-    const mockPool = { connect: jest.fn().mockResolvedValue(mockClient) } as unknown as Pool;
-    const mockHandler = jest.fn().mockRejectedValue(error);
+    const mockFunc = jest.fn().mockRejectedValue(error);
 
-    await tryCatchWrapperWithDb(mockPool)(mockHandler)(req, res, next, mockClient);
+    await tryCatchWrapperWithDb(pool)(mockFunc)(req, res, next);
 
+    expect(pool.connect).toHaveBeenCalled();
+    expect(mockFunc).toHaveBeenCalledWith(req, res, next, mockClient);
     expect(next).toHaveBeenCalledWith(error);
     expect(mockClient.release).toHaveBeenCalled();
   });
 });
 
 describe("tryCatchWrapperWithDbTransaction", () => {
-  it("트랜잭션 정상 흐름", async () => {
-    const mockClient = {
-      query: jest.fn(),
-      release: jest.fn(),
-    } as unknown as PoolClient;
+  it("트랜잭션이 정상적으로 완료되는 경우 next를 호출되지 않아야 한다.", async () => {
+    const mockFunc = jest.fn().mockResolvedValue(undefined);
 
-    const mockPool = {
-      connect: jest.fn().mockResolvedValue(mockClient),
-    } as unknown as Pool;
+    await tryCatchWrapperWithDbTransaction(pool)(mockFunc)(req, res, next);
 
-    const mockHandler = jest.fn().mockResolvedValue(undefined);
-
-    await tryCatchWrapperWithDbTransaction(mockPool)(mockHandler)(req, res, next, mockClient);
-
+    expect(pool.connect).toHaveBeenCalled();
     expect(mockClient.query).toHaveBeenCalledWith("BEGIN");
-    expect(mockHandler).toHaveBeenCalled();
+    expect(mockFunc).toHaveBeenCalledWith(req, res, next, mockClient);
     expect(mockClient.query).toHaveBeenCalledWith("COMMIT");
+    expect(next).not.toHaveBeenCalled();
     expect(mockClient.release).toHaveBeenCalled();
   });
 
-  it("트랜잭션 중 에러 발생 시 ROLLBACK 호출", async () => {
-    const mockClient = {
-      query: jest.fn(),
-      release: jest.fn(),
-    } as unknown as PoolClient;
-
-    const mockPool = {
-      connect: jest.fn().mockResolvedValue(mockClient),
-    } as unknown as Pool;
-
+  it("트랜잭션 중 에러 발생 시 ROLLBACK와 next가 호출되어야 한다.", async () => {
     const error = new Error("transaction error");
-    const mockHandler = jest.fn().mockRejectedValue(error);
+    const mockFunc = jest.fn().mockRejectedValue(error);
 
-    await tryCatchWrapperWithDbTransaction(mockPool)(mockHandler)(req, res, next, mockClient);
+    await tryCatchWrapperWithDbTransaction(pool)(mockFunc)(req, res, next);
 
+    expect(pool.connect).toHaveBeenCalled();
+    expect(mockClient.query).toHaveBeenCalledWith("BEGIN");
+    expect(mockFunc).toHaveBeenCalledWith(req, res, next, mockClient);
     expect(mockClient.query).toHaveBeenCalledWith("ROLLBACK");
     expect(next).toHaveBeenCalledWith(error);
     expect(mockClient.release).toHaveBeenCalled();
   });
 
-  it("트랜잭션 중 에러 발생 시 ROLLBACK 호출 중 오류 발생", async () => {
+  it("트랜잭션 중 에러 발생하여 ROLLBACK 호출 중 오류가 발생한 경우 console.error와 next가 호출되어야 한다.", async () => {
     const rollbackError = new Error("rollback error");
-    const mockQuery = jest
+    mockClient.query = jest
       .fn()
       .mockResolvedValueOnce(undefined)
       .mockRejectedValueOnce(rollbackError);
 
-    const mockClient = {
-      query: mockQuery,
-      release: jest.fn(),
-    } as unknown as PoolClient;
-
-    const mockPool = {
-      connect: jest.fn().mockResolvedValue(mockClient),
-    } as unknown as Pool;
-
     const error = new Error("transaction error");
-    const mockHandler = jest.fn().mockRejectedValue(error);
+    const mockFunc = jest.fn().mockRejectedValue(error);
 
     const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(() => {});
 
-    await tryCatchWrapperWithDbTransaction(mockPool)(mockHandler)(req, res, next, mockClient);
+    await tryCatchWrapperWithDbTransaction(pool)(mockFunc)(req, res, next);
 
+    expect(pool.connect).toHaveBeenCalled();
+    expect(mockClient.query).toHaveBeenCalledWith("BEGIN");
+    expect(mockFunc).toHaveBeenCalledWith(req, res, next, mockClient);
     expect(mockClient.query).toHaveBeenCalledWith("ROLLBACK");
     expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
     expect(next).toHaveBeenCalledWith(error);
