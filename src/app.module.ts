d9e1492fe36559e578bf5467f064ded089d51d8d
blueGyu <bluegyufordev@gmail.com>
2025-09-05 01:01:04 +0900

    feat: 환경 변수 검증 로직 추가
    - ConfigModule을 통한 환경 변수 검증 로직 구현
    - 환경별 .env 파일 로딩 설정 (.env.${NODE_ENV})

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
+import { ConfigModule } from "@nestjs/config";
+import { z } from "zod";
+
+const validateSchema = z.object({
+  DATABASE_URL: z.string(),
+  CRYPTO_ALGORITHM: z.string(),
+  CRYPTO_PASSWORD: z.string(),
+});
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      validate: (config) => {
+        const result = validateSchema.safeParse(config);
+        if (!result.success) {
+          throw new Error(result.error.message);
+        }
+        return result.data;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
